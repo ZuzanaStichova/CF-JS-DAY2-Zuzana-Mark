6efa863237f4b8a7fcec09fedfb6eeafbf11660d
Zuzana Stichova <z.stichova@gmail.com>
2020-09-21 11:47:50 +0200

    with exercise 3

--- basic.js
+++ basic.js
@@ -1,12 +1,12 @@
 //Exercise 1
-var temperature = Math.floor((Math.random()*31) -5);
-console.log(temperature);
+// var temperature = Math.floor((Math.random()*31) -5);
+// console.log(temperature);
 
-if (temperature < 10) {
-    console.log("The weather is cold")
-} else {
-    console.log("The weather is moderate")
-}
+// if (temperature < 10) {
+//     console.log("The weather is cold")
+// } else {
+//     console.log("The weather is moderate")
+// }
 
 //Exercis 2
 var numbers = [1, 7, -3, 9, 2, 0, 100, 50];
@@ -26,3 +26,21 @@ for (i=1; i<numbers.length;i++) {
 console.log(temp)
 
 //Exercise 3
+var temperature = Math.floor((Math.random()*31) -5);
+console.log(temperature);
+
+var img = document.createElement("img");
+img.src = "pic_1.jpg";
+var src = document.getElementById("image"); 
+
+
+
+if (temperature < 0) {
+    document.write(temperature + " is too cold.") 
+    src.appendChild(img)
+} else if (temperature < 20) {
+        document.write(temperature + " is fine")
+    } else {
+        document.write(temperature + " is too high")
+    }
+
