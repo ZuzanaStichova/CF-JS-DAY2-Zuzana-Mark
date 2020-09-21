2a8ae8b1ef21eb5719585d19b27013048a963472
Zuzana Stichova <z.stichova@gmail.com>
2020-09-21 10:44:44 +0200

    Basic exercise 1

---
+++ basic.js
@@ -0,0 +1,8 @@
+var temperature = Math.floor((Math.random()*31) -5);
+console.log(temperature);
+
+if (temperature < 10) {
+    console.log("The weather is cold")
+} else {
+    console.log("The weather is moderate")
+}
